f272d47c44df24ec279ae3d0e33b1349a2e57f4f
Kelvin-Machado <kelvinmachado@live.com>
2019-03-01 16:18:42 -0300

    Finalizado

---
+++ s11ExeFix/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace s11ExeFix.Entities.Exceptions
+{
+    public class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base (message)
+        {
+        }
+    }
+}
