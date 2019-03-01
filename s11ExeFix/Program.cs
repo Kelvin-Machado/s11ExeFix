ed4695b45fa21dd41d36b57644c3d134f1c5c29a
Kelvin-Machado <kelvinmachado@live.com>
2019-03-01 14:30:26 -0300

    First Commit

---
+++ s11ExeFix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace s11ExeFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
